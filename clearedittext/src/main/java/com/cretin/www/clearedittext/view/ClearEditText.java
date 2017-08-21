4115e2eb55edb48dd8c22c0ff6bedad5ae0986a3
MZCretin <mxnzp_life@163.com>
2017-08-21 17:23:49 +0800

    换资源文件

--- clearedittext/src/main/java/com/cretin/www/clearedittext/view/ClearEditText.java
+++ clearedittext/src/main/java/com/cretin/www/clearedittext/view/ClearEditText.java
@@ -1,3 +1,4 @@
+
 package com.cretin.www.clearedittext.view;
 
 import android.content.Context;
@@ -5,6 +6,7 @@ import android.content.res.TypedArray;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
+import android.graphics.Matrix;
 import android.graphics.Paint;
 import android.graphics.Rect;
 import android.graphics.RectF;
@@ -12,6 +14,7 @@ import android.text.Editable;
 import android.text.TextUtils;
 import android.text.TextWatcher;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.widget.EditText;
 
@@ -23,11 +26,13 @@ import com.cretin.www.clearedittext.R;
  */
 
 public class ClearEditText extends EditText {
-    private static final float DEFAUT_SCALE = 0.5f;
+    private static final float DEFAUT_SCALE = 0.4f;
     private Bitmap mClearBitmap;
     private Paint mPaint;
     private int mWidth;
     private int mHeight;
+    private int mBitWidth;
+    private int mBitHeight;
     private boolean showClose;
     private float scale;
     private float padding;
@@ -55,16 +60,18 @@ public class ClearEditText extends EditText {
             try {
                 //获得属性值
                 clearIcon = a.getResourceId(R.styleable.ClearEditText_clearIcon, 0);
-                scale = a.getResourceId(R.styleable.ClearEditText_scaleSize, 0);
+                scale = a.getFloat(R.styleable.ClearEditText_scaleSize, 0);
             } finally { //回收这个对象
                 a.recycle();
             }
         }
         //设置删除图标
+        BitmapFactory.Options bfoOptions = new BitmapFactory.Options();
+        bfoOptions.inScaled = false;
         if ( clearIcon != 0 ) {
-            mClearBitmap = BitmapFactory.decodeResource(getResources(), clearIcon);
+            mClearBitmap = BitmapFactory.decodeResource(getResources(), clearIcon, bfoOptions);
         } else
-            mClearBitmap = BitmapFactory.decodeResource(getResources(), R.mipmap.delete);
+            mClearBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.clear, bfoOptions);
 
         if ( scale == 0 ) {
             scale = DEFAUT_SCALE;
@@ -115,8 +122,8 @@ public class ClearEditText extends EditText {
         super.onDraw(canvas);
 
         if ( showClose ) {
-            Rect mSrcRect = new Rect(0, 0, mHeight, mHeight);
-            RectF mDestRect = new RectF(mWidth - mHeight + padding,
+            Rect mSrcRect = new Rect(0, 0, mBitWidth, mBitWidth);
+            RectF mDestRect = new RectF(mWidth - mBitWidth - padding,
                     padding, mWidth - padding, mHeight - padding);
             canvas.drawBitmap(mClearBitmap, mSrcRect, mDestRect, mPaint);
         }
@@ -160,10 +167,38 @@ public class ClearEditText extends EditText {
         }
         mWidth = width;
         mHeight = height;
+
+        Log.e("HHHHH", "mWidth mHeight " + mWidth + " " + mHeight);
         //MUST CALL THIS
 
         //计算偏移量
         padding = (( float ) mHeight) * (1 - scale) / 2;
+        //第一次进来的时候对图片进行处理
+        deal();
         setMeasuredDimension(width, height);
     }
+
+    private boolean hasScale;
+
+    private void deal() {
+        if ( !hasScale ) {
+            int width = mClearBitmap.getWidth();
+            int height = mClearBitmap.getHeight();
+            Log.e("HHHHH", "width height " + width + " " + height);
+            // 设置想要的大小
+            mBitWidth = ( int ) (mHeight - 2 * padding);
+            mBitHeight = mBitWidth;
+            Log.e("HHHHH", "mBitWidth mBitHeight " + mBitWidth + " " + mBitHeight);
+            // 计算缩放比例
+            float scaleWidth = (( float ) mBitWidth) / width;
+            float scaleHeight = (( float ) mBitHeight) / height;
+            // 取得想要缩放的matrix参数
+            Matrix matrix = new Matrix();
+            matrix.postScale(scaleWidth, scaleHeight);
+            // 得到新的图片
+            mClearBitmap = Bitmap.createBitmap(mClearBitmap, 0, 0, width, height, matrix, true);
+            hasScale = true;
+        }
+    }
 }
+
